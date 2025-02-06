d478477b7834ef4bc42cb9a31b8443053a2dcb7c
Dean Denaki <sonicmailfaster@gmail.com>
2025-02-06 10:35:31 +0700

    new 3

--- lib/camera.dart
+++ lib/camera.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class AbsentScreen extends StatelessWidget {
-  const AbsentScreen({super.key});
+class CameraScreen extends StatelessWidget {
+  const CameraScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
